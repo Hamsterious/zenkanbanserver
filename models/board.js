710f5561d16071103f67dca351d0ab7a09cdf716
martin_hammer <martin_hammer@live.dk>
2017-03-28 13:33:04 +0200

    CRUD functions now work

--- models/board.js
+++ models/board.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var BoardSchema = new mongoose.Schema({
-    id: Number,
     title: String
 });
 
--- routes/boards.js
+++ routes/boards.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Board = require('../models/board.js');
 
-//
+// GET ALL
 router.get('/boards', function(req, res, next) {
     Board.find(function(err, boards) {
         if (err) return next(err);
@@ -14,8 +14,33 @@ router.get('/boards', function(req, res, next) {
     });
 });
 
+// CREATE
 router.post('/boards', function(req, res, next) {
     Board.create(req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+// GET BY ID
+router.get('/boards/:id', function(req, res, next) {
+    Board.findById(req.params.id, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+// UPDATE
+router.post('/boards/:id', function(req, res, next) {
+    Board.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+// DELETE
+router.post('/boards/delete/:id', function(req, res, next) {
+    Board.findByIdAndRemove(req.params.id, req.body, function(err, post) {
         if (err) return next(err);
         res.json(post);
     });
